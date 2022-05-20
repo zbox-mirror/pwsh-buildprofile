85833aeef3236ee7a26a1dabef29eb09845dcc86
Kitsune Solar <kitsune.solar@gmail.com>
2022-05-20 22:17:51 +0300

    2022-05-20 19:17:51

---
+++ install.ps1
@@ -0,0 +1,96 @@
+<#
+.SYNOPSIS
+  Profile installation script.
+.DESCRIPTION
+  The script helps to set up a user profile and install apps.
+#>
+
+#Requires -Version 7.2
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# INITIALIZATION.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+function Start-BuildProfile() {
+  # Run.
+  New-BuildProfile
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# BUILD IMAGE.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+function New-BuildProfile() {
+  # Directories.
+  $d_app        = "D:\Apps"
+  $d_docs       = "D:\Documents"
+  $d_downloads  = "D:\Downloads"
+  $d_music      = "D:\Music"
+  $d_pictures   = "D:\Pictures"
+  $d_torrents   = "D:\Torrents"
+  $d_videos     = "D:\Videos"
+
+  # Check directories.
+  Write-Msg -Title -Message "--- Check & Create Directories on Disk D:..."
+  if ( ! ( Test-Path "$($d_app)" ) ) { New-Item -Path "$($d_app)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_docs)" ) ) { New-Item -Path "$($d_docs)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_downloads)" ) ) { New-Item -Path "$($d_downloads)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_music)" ) ) { New-Item -Path "$($d_music)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_pictures)" ) ) { New-Item -Path "$($d_pictures)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_torrents)" ) ) { New-Item -Path "$($d_torrents)" -ItemType "Directory" }
+  if ( ! ( Test-Path "$($d_videos)" ) ) { New-Item -Path "$($d_videos)" -ItemType "Directory" }
+
+  # Extract apps.
+  Write-Msg -Title -Message "--- Extract Apps..."
+  Expand-7z -In "$($PSScriptRoot)\Apps\Far\Far.7z" -Out "$($d_app)"
+  # Expand-7z -In "$($PSScriptRoot)\Apps\Git\Git.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\KiTTY\KiTTY.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\MPC-HC\MPC-HC.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\OBS-Studio\OBS-Studio.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\PHP\PHP.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\Tixati\Tixati.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\VSCode\VSCode.7z" -Out "$($d_app)"
+  Expand-7z -In "$($PSScriptRoot)\Apps\OpenSSL\OpenSSL.7z" -Out "$($d_app)"
+
+  # Copy settings.
+  # Write-Msg -Title -Message "--- Copy Settings..."
+  # Copy-Item "$($PSScriptRoot)\Docs\Git\.gitconfig" -Destination "$($Env:USERPROFILE)"
+  # Copy-Item "$($PSScriptRoot)\Docs\Git\.git-credentials" -Destination "$($Env:USERPROFILE)"
+
+  # Modification PATH variable.
+  Write-Msg -Title -Message "--- Modification PATH variable..."
+  # $Env:Path += ";$($d_app)\Git"
+  $Env:Path += ";$($d_app)\PHP"
+  $Env:Path += ";$($d_app)\OpenSSL"
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------< COMMON FUNCTIONS >------------------------------------------------ #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+function Write-Msg() {
+  param (
+    [string]$Message,
+    [switch]$Title = $false
+  )
+  if ( $Title ) {
+    Write-Host "$($Message)" -ForegroundColor Blue
+  } else {
+    Write-Host "$($Message)"
+  }
+}
+
+function Expand-7z() {
+  param (
+    [string]$In,
+    [string]$Out
+  )
+  $7zParams = "x", "$($In)", "-o$($Out)", "-aoa"
+  & "$($PSScriptRoot)\_META\7z\7za.exe" @7zParams
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< INIT FUNCTIONS >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+Start-BuildProfile
