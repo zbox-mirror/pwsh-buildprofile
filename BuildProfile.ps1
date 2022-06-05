96be1760b25a5896883d5a965df8cd5060e500d1
Kitsune Solar <kitsune.solar@gmail.com>
2022-06-05 03:05:48 +0300

    2022-06-05 00:05:48

--- BuildProfile.ps1
+++ BuildProfile.ps1
@@ -14,19 +14,19 @@ Param(
   )]
   [ValidatePattern("^[A-Z]$")]
   [Alias("DL")]
-  [string]$DriveLetter,
+  [string]$P_DriveLetter,
 
   [Parameter(HelpMessage="Install applications.")]
   [Alias("IA")]
-  [switch]$InstallApps = $false,
+  [switch]$P_InstallApps = $false,
 
   [Parameter(HelpMessage="Install documents.")]
   [Alias("ID")]
-  [switch]$InstallDocs = $false,
+  [switch]$P_InstallDocs = $false,
 
   [Parameter(HelpMessage="Install PATH variable.")]
   [Alias("IP")]
-  [switch]$InstallPath = $false
+  [switch]$P_InstallPath = $false
 )
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -35,13 +35,13 @@ Param(
 
 function Start-BuildProfile() {
   # Directories.
-  $D_APPS       = "$($DriveLetter):\Apps"
-  $D_DOCS       = "$($DriveLetter):\Documents"
-  $D_DOWNLOADS  = "$($DriveLetter):\Downloads"
-  $D_MUSIC      = "$($DriveLetter):\Music"
-  $D_PICTURES   = "$($DriveLetter):\Pictures"
-  $D_TORRENTS   = "$($DriveLetter):\Torrents"
-  $D_VIDEOS     = "$($DriveLetter):\Videos"
+  $D_APPS       = "$($P_DriveLetter):\Apps"
+  $D_DOCS       = "$($P_DriveLetter):\Documents"
+  $D_DOWNLOADS  = "$($P_DriveLetter):\Downloads"
+  $D_MUSIC      = "$($P_DriveLetter):\Music"
+  $D_PICTURES   = "$($P_DriveLetter):\Pictures"
+  $D_TORRENTS   = "$($P_DriveLetter):\Torrents"
+  $D_VIDEOS     = "$($P_DriveLetter):\Videos"
 
   # New line separator.
   $NL = [Environment]::NewLine
@@ -59,13 +59,13 @@ function Start-BuildData() {
   Start-BPInstallDirs
 
   # Install applications.
-  if ( $InstallApps ) { Start-BPInstallApps }
+  if ( $P_InstallApps ) { Start-BPInstallApps }
 
   # Install documents.
-  if ( $InstallDocs ) { Start-BPInstallDocs }
+  if ( $P_InstallDocs ) { Start-BPInstallDocs }
 
   # Install PATH variable.
-  if ( $InstallPath ) { Start-BPInstallPath }
+  if ( $P_InstallPath ) { Start-BPInstallPath }
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
